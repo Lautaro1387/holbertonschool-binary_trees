2e5bfed3c75c44dcd6da814a7458f52e28a426d0
Lautaro1387 <lautaro12387@gmail.com>
2022-08-31 13:36:50 -0700

    Function that insert a node the right-child of another node

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *right_node = NULL, *aux = NULL;
+
+	if (!parent)
+		return (NULL);
+
+	right_node = binary_tree_node(parent, value);
+	if (right_node)
+	{
+		aux = parent->right;
+		if (!aux)
+			parent->right = right_node;
+		else
+		{
+			aux->parent = right_node;
+			right_node->right = aux;
+			parent->right = right_node;
+		}
+	}
+	return (right_node);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,13 @@
 #ifndef BINARY_TREE
 #define BINARY_TREE
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <unistd.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -8,13 +16,13 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
+} binary_tree_t;
 typedef struct binary_tree_s binary_tree_t;
 /** Binary Search Tree */
 typedef struct binary_tree_s bst_t;
@@ -23,6 +31,8 @@ typedef struct binary_tree_s avl_t;
 /** Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 /** Prototypes */
+
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);

39ec2817b6a6bf7813eb3f867ad6678d7762dc7f
Imarino4 <ignacio.martacos@gmail.com>
2022-09-02 10:30:28 -0700

    Task 13 - betty checked

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,10 +11,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	size_t nodes = 1;
 
 	if (!tree)
-		return 0;
+		return (0);
 	if (!tree->left && !tree->right)
 		return (0);
 	nodes = nodes + binary_tree_nodes(tree->left);
 	nodes = nodes + binary_tree_nodes(tree->right);
-	return(nodes);
+	return (nodes);
 }

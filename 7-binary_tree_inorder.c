2af2f5e2732bbaf84038ea75f1afcc6df087efdb
Imarino4 <ignacio.martacos@gmail.com>
2022-09-02 10:14:22 -0700

    Task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -11,6 +11,6 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree)
 		return;
 	binary_tree_inorder(tree->left, func);
-	func(aux->n);
+	func(tree->n);
 	binary_tree_inorder(tree->right, func);	
 }

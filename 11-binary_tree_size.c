ec19128ac6e6a1777839165c94104f6a8ee30147
Imarino4 <ignacio.martacos@gmail.com>
2022-09-02 10:27:47 -0700

    Task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,6 +3,7 @@
 /**
  * binary_tree_size - measures the size of a binary tree
  * @tree: pointer to root
+ * Return: The size, 0 otherwhise
  */
 
 size_t binary_tree_size(const binary_tree_t *tree)

2e77b27f9843784d1e240f40f46d9f8186cb403f
Imarino4 <ignacio.martacos@gmail.com>
2022-09-02 11:26:23 -0700

    Task 15 - betty checked

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,6 +13,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree->left && !tree->right)
 		return (1);
 	if (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right))
-		return(1);
+		return (1);
 	return (0);
 }
